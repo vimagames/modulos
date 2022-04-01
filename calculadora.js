953dd55315f2938eaa9d3932453e37935de7563c
Nata <natadsouza4@gmail.com>
2022-04-01 10:34:45 -0300

    1.0.0

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+var somar = require("./modulos/somar");
+var subtracao = require("./modulos/subtracao");
+var divisao = require("./modulos/divisao");
+var multiplicao = require("./modulos/multiplicao");
+
+console.log(multiplicao(2,2));
+console.log(divisao(4,2));
+console.log(subtracao(10,7));
+console.log(somar(20,90));
